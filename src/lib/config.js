564c67dcceef2d4c551c8f5f7f0ec667c0a70e72
Bhaskar <bhaskarsoni588@gmail.com>
2024-03-15 11:46:29 +0530

    Title: Secure api calling

--- src/lib/config.js
+++ src/lib/config.js
@@ -1,4 +1,4 @@
-export const GEO_BASE_URL = "http://api.openweathermap.org";
+export const GEO_BASE_URL = "https://api.openweathermap.org";
 export const FORECAST_URL = "https://api.weatherbit.io"
 export const GEO_API_KEY = "40537396157d7cf61b343a6150c2a20f";
 export const FORECAST_API_KEY = "8ff6b1c427824112b02b9f92f1485bbb";
